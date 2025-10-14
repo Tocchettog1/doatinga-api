cf877bbecccb75aae203c2f7088856a4c97c3633
Tocchettog1 <miguelpereira.dev@gmail.com>
2025-10-14 13:33:44 -0300

    feat: DB Connection

--- knexfile.js
+++ knexfile.js
@@ -7,22 +7,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      database: process.env.DB_DATABASE,
-      password: process.env.DB_PASSWORD,
-      ssl: { rejectUnauthorized: false }
-    },
-    migrations: {
-      directory: path.join(__dirname, 'src', 'data', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'src', 'data', 'seeds')
-    }
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    ssl: { rejectUnauthorized: false }
+  },
+  migrations: {
+    directory: path.join(__dirname, 'src', 'data', 'migrations')
+  },
+  seeds: {
+    directory: path.join(__dirname, 'src', 'data', 'seeds')
   }
 };
 
