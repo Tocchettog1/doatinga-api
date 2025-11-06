757e7575226fec6772613d106cb0a19d33ba1f6c
Leonardo <leoandrighetto02@gmail.com>
2025-11-06 19:39:23 -0300

    Update GetAll(Institutions)

---
+++ src/controllers/InstitutionsController.js
@@ -0,0 +1,33 @@
+import db from "../data/database.js";
+import AppError from "../utils/AppError.js";
+import { toCamelCase } from "../utils/format.js";
+
+// Busca de todas as linhas e aplicar filtros
+
+export default {
+    async getAll(req, res, next) {
+        try {
+            const { name } = req.query;
+            
+            //const query = req.query;
+            let institutions = await db('institutions').select()
+                .where((builder) => {
+                    if (name){
+                        builder.where('name','LIKE',`${name}%`)
+                    }
+                })
+
+            institutions = toCamelCase(institutions);
+            
+            return res.status(200).json({
+                status: true,
+                data: institutions
+            });
+
+        } catch (error) {
+            next(error);
+        }
+    },
+
+}
+
