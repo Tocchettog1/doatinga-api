37f1ad7ffdcc42997085a867bf33492ad5685bf9
Leonardo <leoandrighetto02@gmail.com>
2025-11-06 20:03:57 -0300

    feat: implementação de GETALL em Instituições, e rotas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import exampleRouter from './exampleRoute.js';
 
+import InstitutionsRouter from './InstitutionsRoute.js';
+
 const routes = express.Router();
 
 routes.use(exampleRouter);
+
+routes.use('/Institutions', InstitutionsRouter)
 
 export default routes;
